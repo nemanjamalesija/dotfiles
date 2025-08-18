b85e20356b9e2dcb95ec069f93a07a4c3b180123
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-08-18 09:30:52 +0200

    Update config

---
+++ nvim/lua/plugins/diffview.lua
@@ -0,0 +1,15 @@
+return {
+    "sindrets/diffview.nvim",
+    cmd = {
+        "DiffviewOpen",
+        "DiffviewFileHistory",
+        "DiffviewFocusFiles",
+        "DiffviewToggleFiles",
+        "DiffviewRefresh",
+        "DiffviewLog",
+    },
+    -- keys = {
+    --     { "<leader>gf", "<Cmd>DiffviewFileHistory<CR>", desc = "Git file history" },
+    --     { "<leader>gd", "<Cmd>DiffviewOpen<CR>", desc = "Git diff this (DiffHistory)" },
+    -- },
+}
