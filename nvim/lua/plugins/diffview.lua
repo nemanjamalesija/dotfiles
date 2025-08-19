56e71c1434508d4e0cc3dcaf739bb1c0ad15cf02
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-08-19 08:29:08 +0200

    Update diffview

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -8,4 +8,9 @@ return {
         "DiffviewRefresh",
         "DiffviewLog",
     },
+    keys = {
+        { "<leader>gf", "<Cmd>DiffviewFileHistory %<CR>", desc = "Git Current File History" },
+        { "<leader>go", "<Cmd>DiffviewOpen<CR>", desc = "Open Git Diff Current File" },
+        { "<leader>gc", "<Cmd>DiffviewClose<CR>", desc = "Close Git Diff Current File" },
+    },
 }
