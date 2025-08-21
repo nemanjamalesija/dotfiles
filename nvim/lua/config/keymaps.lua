c0571c159f088f2a8f30dd09f35b63669161da35
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-08-21 09:04:32 +0200

    Add text objects keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -29,4 +29,41 @@ vim.keymap.set("n", "<leader>my", function()
     vim.schedule(function()
         require("cmp").complete()
     end)
-end, { desc = "Manual completion (nvim-cmp)" })
+end)
+
+local function dive_with_count_object()
+    local count = vim.v.count1 -- Gets the count prefix (defaults to 1)
+    local original_pos = vim.fn.getpos(".")
+
+    -- Jump to the nth occurrence of {
+    for i = 1, count do
+        local found = vim.fn.search("{", "W")
+        if found == 0 then
+            vim.fn.setpos(".", original_pos)
+            print("Only found " .. (i - 1) .. " blocks forward")
+            return
+        end
+    end
+
+    vim.cmd("normal! vi{")
+end
+
+local function dive_with_count_paren()
+    local count = vim.v.count1
+    local original_pos = vim.fn.getpos(".")
+
+    -- Jump to the nth occurrence of (
+    for i = 1, count do
+        local found = vim.fn.search("(", "W")
+        if found == 0 then
+            vim.fn.setpos(".", original_pos)
+            print("Only found " .. (i - 1) .. " parentheses forward")
+            return
+        end
+    end
+
+    vim.cmd("normal! vi(")
+end
+
+vim.keymap.set("n", "vi{", dive_with_count_object, { desc = "Dive inside nth { block" })
+vim.keymap.set("n", "vi(", dive_with_count_paren, { desc = "Dive inside nth ( block" })
