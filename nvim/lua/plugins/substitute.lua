7febf48645c9dbc33b4d536273394e709eb4f4bc
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-08-21 09:05:31 +0200

    Update substitute key bindings

--- nvim/lua/plugins/substitute.lua
+++ nvim/lua/plugins/substitute.lua
@@ -7,16 +7,18 @@ return {
             },
             range = {
                 group_substituted_text = false,
-                prefix = "s",
+                prefix = "r",
                 prompt_current_text = false,
                 suffix = "",
             },
         },
         init = function()
-            vim.keymap.set("n", "r", require("substitute").operator, {})
-            vim.keymap.set("n", "rr", require("substitute").line, {})
-            vim.keymap.set("n", "R", require("substitute").eol, {})
-            vim.keymap.set("x", "r", require("substitute").visual, {})
+            local substitute = require("substitute")
+
+            vim.keymap.set("n", "<leader>r", substitute.operator, { noremap = true, desc = "Substitute operator" })
+            vim.keymap.set("n", "<leader>rr", substitute.line, { noremap = true, desc = "Substitute line" })
+            vim.keymap.set("n", "<leader>R", substitute.eol, { noremap = true, desc = "Substitute to end of line" })
+            vim.keymap.set("x", "<leader>r", substitute.visual, { noremap = true, desc = "Substitute visual" })
 
             vim.api.nvim_set_hl(0, "SubstituteSubstituted", { link = "Substitute" })
             vim.api.nvim_set_hl(0, "SubstituteRange", { link = "Substitute" })
