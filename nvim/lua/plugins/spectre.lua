3a0ea0f404626fad2c17257ec4b69f839dcf0933
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-08-22 09:15:07 +0200

    Update spectre config

--- nvim/lua/plugins/spectre.lua
+++ nvim/lua/plugins/spectre.lua
@@ -7,18 +7,63 @@ return {
             "nvim-lua/plenary.nvim",
             "catppuccin/nvim",
         },
+        keys = {
+            -- Global search and replace
+            {
+                "<leader>S",
+                function()
+                    require("spectre").toggle()
+                end,
+                desc = "Toggle Spectre",
+            },
+
+            -- Search current word globally
+            {
+                "<leader>sw",
+                function()
+                    require("spectre").open_visual({ select_word = true })
+                end,
+                desc = "Search current word",
+            },
+
+            -- Search current file only
+            {
+                "<leader>sf",
+                function()
+                    require("spectre").open_file_search({
+                        select_word = true,
+                        is_regex = false, -- Start with literal search
+                    })
+                end,
+                desc = "Search in current file",
+            },
+
+            -- Search current file with current word
+            {
+                "<leader>sF",
+                function()
+                    require("spectre").open_file_search({
+                        select_word = true,
+                        is_regex = false,
+                    })
+                end,
+                desc = "Search current word in file",
+            },
+
+            -- Visual mode search
+            {
+                "<leader>sw",
+                function()
+                    require("spectre").open_visual()
+                end,
+                mode = "v",
+                desc = "Search current selection",
+            },
+        },
         config = function()
             local theme = require("catppuccin.palettes").get_palette("macchiato")
-
             vim.api.nvim_set_hl(0, "SpectreSearch", { bg = theme.red, fg = theme.base })
             vim.api.nvim_set_hl(0, "SpectreReplace", { bg = theme.green, fg = theme.base })
-
-            require("spectre").setup({
-                highlight = {
-                    search = "SpectreSearch",
-                    replace = "SpectreReplace",
-                },
-            })
         end,
     },
 }
