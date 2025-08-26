1b5125dae0c24110c17feff1c634f408fdd26958
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-08-26 13:14:13 +0200

    Update config

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -3,4 +3,9 @@ return {
     { "echasnovski/mini.animate", opts = { cursor = { enable = false } } },
     { "echasnovski/mini.icons", enabled = false },
     { "echasnovski/mini.bufremove", version = false },
+    { "echasnovski/mini.surround", enabled = false },
+    { "echasnovski/mini.splitjoin", enabled = false },
+    { "echasnovski/mini.operators", enabled = false },
+    { "echasnovski/mini.align", enabled = false },
+    { "echasnovski/mini.ai", enabled = false },
 }
--- nvim/lua/plugins/surround.lua
+++ nvim/lua/plugins/surround.lua
@@ -2,6 +2,7 @@
 --------------------------------------------------------------------------------
     surr*ound_words             ysiw)           (surround_words)
     *make strings               ys$"            "make strings"
+    [delete ar*ound me!]        ds]             delete around me!
     remove <b>HTML t*ags</b>    dst             remove HTML tags
     'change quot*es'            cs'"            "change quotes"
     <b>or tag* types</b>        csth1<CR>       <h1>or tag types</h1>
@@ -12,20 +13,6 @@ return {
     version = "^3.0.0",
     event = "VeryLazy",
     config = function()
-        require("nvim-surround").setup({
-            keymaps = {
-                insert = "<C-g>s",
-                insert_line = "<C-g>S",
-                normal = "s",
-                normal_cur = "sl",
-                normal_line = "sS",
-                normal_cur_line = "sSS",
-                visual = "sv",
-                visual_line = "svl",
-                delete = "ds",
-                change = "cs",
-                change_line = "cS",
-            },
-        })
+        require("nvim-surround").setup({})
     end,
 }
