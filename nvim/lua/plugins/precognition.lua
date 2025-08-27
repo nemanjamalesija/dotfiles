a086fdaeebe1b31e4ccdf3c7f4560382d8561b42
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-08-27 08:52:02 +0200

    Remove precognition

--- nvim/lua/plugins/precognition.lua
+++
@@ -1,8 +0,0 @@
-return {
-    -- "tris203/precognition.nvim",
-    -- config = function()
-    --     require("precognition").setup({
-    --         startVisible = true, -- This makes hints visible by default
-    --     })
-    -- end,
-}
