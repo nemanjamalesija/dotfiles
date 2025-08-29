1a7cede761c8f8f423f42a8d817abea2b1d25890
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-08-29 09:12:08 +0200

    Update harpoon

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -16,27 +16,15 @@ return {
             harpoon.ui:toggle_quick_menu(harpoon:list())
         end, { desc = "Toggle Harpoon menu" })
 
+        vim.keymap.set("n", "<leader>hh", function()
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+        end, { desc = "Toggle Harpoon menu" })
+
         -- Fixed select keymaps
         for i = 1, 5 do
             vim.keymap.set("n", "<M-" .. i .. ">", function()
                 harpoon:list():select(i)
             end, { desc = "Harpoon to File " .. i })
         end
-
-        -- Optional: Add prev/next navigation
-        vim.keymap.set("n", "<M-l>", function()
-            harpoon:list():prev()
-        end)
-        vim.keymap.set("n", "<M-h>", function()
-            harpoon:list():next()
-        end)
-
-        -- Handle swap file issues
-        vim.api.nvim_create_autocmd("SwapExists", {
-            callback = function()
-                vim.v.swapchoice = "o" -- Open read-only
-                vim.notify("File opened read-only due to existing swap file", vim.log.levels.WARN)
-            end,
-        })
     end,
 }
