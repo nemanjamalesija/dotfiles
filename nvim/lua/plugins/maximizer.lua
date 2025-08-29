6b3d92212d8675474ae955f896d30f0ba86e948e
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-08-29 09:13:29 +0200

    Update maximizer

--- nvim/lua/plugins/maximizer.lua
+++ nvim/lua/plugins/maximizer.lua
@@ -1,8 +1,18 @@
 return {
     {
-        "szw/vim-maximizer",
-        keys = {
-            { "<leader>mt", ":MaximizerToggle<CR>", desc = "Toggle Maximizer" },
+        "anuvyklack/windows.nvim",
+        dependencies = {
+            "anuvyklack/middleclass",
         },
+        config = function()
+            vim.o.winwidth = 10
+            vim.o.winminwidth = 10
+            vim.o.equalalways = true
+            require("windows").setup({
+                autowidth = { enable = false },
+            })
+
+            vim.keymap.set("n", "<M-m>", "<Cmd>WindowsMaximize<CR>", { desc = "Maximize Window Fully" })
+        end,
     },
 }
