6a931ec6250d3135197ced1586ba71aa14f82a43
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-09-04 11:00:23 +0200

    Update lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,3 +1,72 @@
+-- Helper functions for code quality indicators
+local function trailing_space()
+    if not vim.o.modifiable then
+        return ""
+    end
+
+    for i = 1, vim.fn.line("$") do
+        local linetext = vim.fn.getline(i)
+        local idx = vim.fn.match(linetext, [[\v\s+$]])
+        if idx ~= -1 then
+            return string.format("[%d]trailing", i)
+        end
+    end
+    return ""
+end
+
+local function mixed_indent()
+    if not vim.o.modifiable then
+        return ""
+    end
+
+    local space_pat = [[\v^ +]]
+    local tab_pat = [[\v^\t+]]
+    local space_indent = vim.fn.search(space_pat, "nwc")
+    local tab_indent = vim.fn.search(tab_pat, "nwc")
+    local mixed = (space_indent > 0 and tab_indent > 0)
+    local mixed_same_line
+
+    if not mixed then
+        mixed_same_line = vim.fn.search([[\v^(\t+ | +\t)]], "nwc")
+        mixed = mixed_same_line > 0
+    end
+
+    if not mixed then
+        return ""
+    end
+
+    if mixed_same_line ~= nil and mixed_same_line > 0 then
+        return "MI:" .. mixed_same_line
+    end
+
+    local space_indent_cnt = vim.fn.searchcount({ pattern = space_pat, max_count = 1e3 }).total
+    local tab_indent_cnt = vim.fn.searchcount({ pattern = tab_pat, max_count = 1e3 }).total
+
+    if space_indent_cnt > tab_indent_cnt then
+        return "MI:" .. tab_indent
+    else
+        return "MI:" .. space_indent
+    end
+end
+
+local function get_active_lsp()
+    local msg = "🚫"
+    local buf_ft = vim.api.nvim_get_option_value("filetype", {})
+    local clients = vim.lsp.get_clients({ bufnr = 0 })
+
+    if next(clients) == nil then
+        return msg
+    end
+
+    for _, client in ipairs(clients) do
+        local filetypes = client.config.filetypes
+        if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+            return client.name
+        end
+    end
+    return msg
+end
+
 local function diff_source()
     local gitsigns = vim.b.gitsigns_status_dict
     if gitsigns then
@@ -16,10 +85,14 @@ return {
     opts = function(_, opts)
         opts.options = {
             theme = "auto",
-            component_separators = { left = "", right = "" },
-            section_separators = { left = "", right = "" },
+            component_separators = { left = "⏐", right = "⏐" },
+            section_separators = { left = "", right = "" },
             globalstatus = true,
+            refresh = {
+                statusline = 1000,
+            },
         }
+
         opts.sections = {
             lualine_a = { "mode" },
             lualine_b = {
@@ -33,9 +106,16 @@ return {
                         unnamed = "[No Name]",
                         newfile = "[New]",
                     },
-                    shorting_target = 20, -- Not relevant when path=0, but harmless
+                    shorting_target = 20,
                 },
-                -- { "branch", icon = "" },
+                -- {
+                --     "branch",
+                --     fmt = function(name, _)
+                --         -- truncate branch name in case the name is too long
+                --         return string.sub(name, 1, 20)
+                --     end,
+                --     color = { gui = "italic,bold" },
+                -- },
             },
             lualine_c = {
                 {
@@ -44,39 +124,43 @@ return {
                 },
             },
             lualine_x = {
-
+                {
+                    get_active_lsp,
+                },
                 {
                     "diagnostics",
-
-                    -- Table of diagnostic sources, available sources are:
-                    --   'nvim_lsp', 'nvim_diagnostic', 'nvim_workspace_diagnostic', 'coc', 'ale', 'vim_lsp'.
-                    -- or a function that returns a table as such:
-                    --   { error=error_cnt, warn=warn_cnt, info=info_cnt, hint=hint_cnt }
                     sources = { "nvim_diagnostic", "coc" },
-
-                    -- Displays diagnostics for the defined severity types
                     sections = { "error", "warn", "info", "hint" },
-
                     diagnostics_color = {
-                        -- Same values as the general color option can be used here.
-                        error = "DiagnosticError", -- Changes diagnostics' error color.
-                        warn = "DiagnosticWarn", -- Changes diagnostics' warn color.
-                        info = "DiagnosticInfo", -- Changes diagnostics' info color.
-                        hint = "DiagnosticHint", -- Changes diagnostics' hint color.
+                        error = "DiagnosticError",
+                        warn = "DiagnosticWarn",
+                        info = "DiagnosticInfo",
+                        hint = "DiagnosticHint",
                     },
                     symbols = { error = "E", warn = "W", info = "I", hint = "H" },
-                    colored = true, -- Displays diagnostics status in color if set to true.
-                    update_in_insert = false, -- Update diagnostics in insert mode.
-                    always_visible = false, -- Show diagnostics even if there are none.
+                    colored = true,
+                    update_in_insert = false,
+                    always_visible = false,
+                },
+                {
+                    trailing_space,
+                    color = "WarningMsg",
+                },
+                {
+                    mixed_indent,
+                    color = "WarningMsg",
                 },
             },
-            lualine_y = { "progress" },
+            lualine_y = {
+                "progress",
+            },
             lualine_z = {
                 function()
                     return os.date("%H:%M")
                 end,
             },
         }
+
         opts.inactive_sections = {
             lualine_a = {},
             lualine_b = {},
@@ -85,7 +169,7 @@ return {
             lualine_y = {},
             lualine_z = {},
         }
-        -- Add navic to lualine_c if desired
+
         if not vim.g.trouble_lualine then
             table.insert(opts.sections.lualine_c, { "navic", color_correction = "dynamic" })
         end
