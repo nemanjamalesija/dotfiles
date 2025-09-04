be96473339452e82f5b6bb4d3054739d392fde07
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-09-04 10:55:58 +0200

    Update colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -5,90 +5,30 @@ return {
     },
     {
         "catppuccin/nvim",
-        name = "catppuccin-mocha",
-
+        name = "catppuccin",
+        -- priority = 1000,
+        -- lazy = false,
         config = function()
             require("catppuccin").setup({
-                flavour = "mocha",
-                transparent_background = true,
+                flavour = "macchiato",
+                -- transparent_background = true,
                 float = {
                     enabled = true,
-                    transparent = true,
+                    transparent = false,
                     solid = false,
                 },
             })
-            --[[ vim.cmd.colorscheme("catppuccin-mocha") ]]
         end,
     },
     {
         "sainnhe/everforest",
-        lazy = false,
-        priority = 1000,
+        -- lazy = false,
+        -- priority = 1000,
         config = function()
-            vim.g.everforest_background = "medium"
+            vim.g.everforest_background = "hard"
             vim.g.everforest_enable_italic = true
             vim.g.everforest_better_performance = 1
-            vim.cmd.colorscheme("everforest")
-        end,
-    },
-    {
-        "polirritmico/monokai-nightasty.nvim",
-        lazy = true,
-        config = function()
-            require("monokai-nightasty").setup({
-                on_colors = function(colors)
-                    -- subtitute purple for yellow
-                    colors.purple = "#859900"
-                end,
-                on_highlights = function(highlights, colors)
-                    local fg_text = "#6c6c6c"
-                    local bg_poup = "#f9f9f9"
-                    local lsp_ref_bg = "#dddddd"
-                    local sublime_blue = "#2aa198"
-                    local sublime_blue_dark = "#4791e4"
-                    local purple_subtitute_yellow = "#8da101"
-                    local everforest_pink = "#df69ba"
-
-                    highlights.String = { fg = sublime_blue }
-                    highlights.Constant = { fg = sublime_blue }
-
-                    highlights.cssValueNumber = { fg = everforest_pink }
-                    highlights.cssBraces = { fg = fg_text }
-
-                    highlights.sassVariable = { fg = colors.black }
-                    highlights.StorageClass = { fg = fg_text }
-                    highlights.sassClass = { fg = colors.blue }
-                    highlights.sassDefinition = { fg = colors.blue }
-                    highlights.sassCssAttribute = { fg = colors.green }
-                    highlights["@property.scss"] = { fg = fg_text }
-
-                    highlights["@variable"] = { fg = fg_text }
-                    highlights["@variable.member"] = { fg = sublime_blue_dark }
-                    highlights["@punctuation.bracket"] = { fg = fg_text }
-                    highlights["@tag.attribute"] = { fg = purple_subtitute_yellow }
-                    highlights["@lsp"] = { fg = fg_text }
-                    highlights["@variable.builtin"] = { fg = purple_subtitute_yellow, bold = true }
-                    highlights["@constant.builtin"] = { fg = everforest_pink }
-                    highlights["@number"] = { fg = everforest_pink }
-                    highlights["@boolean"] = { fg = everforest_pink }
-
-                    highlights.NormalFloat = { bg = bg_poup }
-                    highlights.FloatBorder = { bg = bg_poup, fg = fg_text }
-
-                    highlights.TelescopeNormal = { bg = bg_poup, fg = fg_text }
-                    highlights.TelescopeBorder = { bg = bg_poup, fg = fg_text }
-
-                    highlights.DiagnosticVirtualTextError = { fg = colors.error, bg = "NONE" }
-                    highlights.DiagnosticVirtualTextWarn = { fg = colors.warning, bg = "NONE" }
-                    highlights.DiagnosticVirtualTextInfo = { fg = colors.info, bg = "NONE" }
-                    highlights.DiagnosticVirtualTextHint = { fg = colors.hint, bg = "NONE" }
-
-                    highlights.LspReferenceText = { bg = lsp_ref_bg }
-                    highlights.LspReferenceRead = { bg = lsp_ref_bg }
-                    highlights.LspReferenceWrite = { bg = lsp_ref_bg }
-                end,
-            })
-            -- vim.cmd.colorscheme("monokai-nightasty")
+            -- vim.cmd.colorscheme("everforest")
         end,
     },
     {
@@ -97,11 +37,27 @@ return {
             vim.o.background = "dark"
             require("vscode").setup({
                 background = "hard",
-                -- group_overrides = {
-                --     ["@variable.parameter"] = { fg = "#FF9900" },
-                -- },
             })
             -- vim.cmd.colorscheme("vscode")
+        end,
+    },
+    {
+        "olivercederborg/poimandres.nvim",
+        config = function()
+            require("poimandres").setup({
+                bold_vert_split = false,
+                dim_nc_background = false,
+                disable_background = false,
+                disable_float_background = false,
+                disable_italics = false,
+            })
+            -- vim.cmd.colorscheme("poimandres")
+        end,
+    },
+    {
+        "rose-pine/neovim",
+        config = function()
+            -- vim.cmd("colorscheme kanagawa-dragon")
         end,
     },
 }
