539593105f85cbb06174d1df6451048e0ebe0b31
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-09-04 10:56:33 +0200

    update noice

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -6,7 +6,6 @@ return {
     "folke/noice.nvim",
     event = "VeryLazy",
     dependencies = {
-        "MunifTanjim/nui.nvim",
         {
             "rcarriga/nvim-notify",
             config = function()
@@ -23,7 +22,6 @@ return {
                     end,
                     render = "default",
                     stages = "fade",
-                    -- Remove or lower the level to show more message types
                     level = vim.log.levels.WARN,
                     merge_duplicates = false,
                 })
@@ -31,30 +29,12 @@ return {
         },
     },
     opts = {
-
         cmdline = {
             enabled = true,
             view = "cmdline",
             format = {
                 search_down = { view = "cmdline" },
                 search_up = { view = "cmdline" },
-            },
-        },
-        views = {
-            notify = {
-                backend = "notify",
-                fallback = "mini",
-                format = "notify",
-                replace = false,
-                merge = false,
-                win_options = {
-                    winblend = 0,
-                    wrap = true,
-                },
-                position = {
-                    row = 1,
-                    col = -2,
-                },
             },
         },
     },
