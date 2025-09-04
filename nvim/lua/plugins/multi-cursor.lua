fff2c116e29424139bc24dffb298f7d0a582a492
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-09-04 10:56:45 +0200

    Update multi cursor

--- nvim/lua/plugins/multi-cursor.lua
+++ nvim/lua/plugins/multi-cursor.lua
@@ -1,10 +1,38 @@
-return {
-    "mg979/vim-visual-multi",
-    branch = "master",
-    init = function()
-        vim.g.VM_maps = {
-            ["Add Cursor Down"] = "<c-j>",
-            ["Add Cursor Up"] = "<c-k>",
-        }
+vim.api.nvim_create_autocmd("ColorScheme", {
+    pattern = "*",
+    callback = function()
+        -- Link multicursor highlights to existing ones from your colorscheme
+        vim.api.nvim_set_hl(0, "MultiCursorMain", { link = "Visual" })
+        vim.api.nvim_set_hl(0, "MultiCursor", { link = "Visual" })
     end,
+})
+return {
+    {
+        "smoka7/multicursors.nvim",
+        event = "VeryLazy",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "Cathyprime/hydra.nvim",
+        },
+        opts = {},
+        cmd = { "MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor" },
+        keys = {
+            {
+                "<Leader>m",
+                "<CMD>MCstart<CR>",
+                desc = "multicursor",
+            },
+            {
+                "<Leader>m",
+                "<CMD>MCvisual<CR>",
+                mode = "v",
+                desc = "multicursor",
+            },
+            {
+                "<c-j>",
+                "<CMD>MCunderCursor<CR>",
+                desc = "multicursor down",
+            },
+        },
+    },
 }
