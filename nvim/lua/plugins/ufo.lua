e08c462fcf59ee8b1da20bed461113401b2c8033
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-09-10 23:48:57 +0200

    Update ufo

--- nvim/lua/plugins/ufo.lua
+++ nvim/lua/plugins/ufo.lua
@@ -1,15 +1,28 @@
 return {
     {
         "kevinhwang91/nvim-ufo",
-        event = "BufEnter",
+        event = "VeryLazy",
         dependencies = {
             "kevinhwang91/promise-async",
         },
         config = function()
-            --- @diagnostic disable: unused-local
             require("ufo").setup({
-                provider_selector = function(_bufnr, _filetype, _buftype)
-                    return { "treesitter", "indent" }
+                provider_selector = function(bufnr, filetype, buftype)
+                    return { "indent", "lsp" }
+                end,
+            })
+
+            vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+            vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+
+            vim.api.nvim_create_autocmd({ "BufReadPost", "BufWinEnter" }, {
+                callback = function()
+                    local filetype = vim.bo.filetype
+                    if filetype ~= "" and vim.bo.buftype == "" then
+                        vim.schedule(function()
+                            pcall(vim.cmd, "UfoEnableFold")
+                        end)
+                    end
                 end,
             })
         end,
