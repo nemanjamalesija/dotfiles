220c94a7657e40b8fa24cb2d61e35a2b7a4c55b4
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-09-14 00:30:57 +0200

    Update keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -9,10 +9,6 @@
 vim.keymap.set("n", "<leader>s", "<cmd>w<cr>", { desc = "Save file" })
 vim.keymap.set("n", "<leader>q", "<cmd>q<cr>", { desc = "Quit buffer" })
 
--- vim.keymap.set("n", "<leader>S", ":%s/", { noremap = true, desc = "Substitute word" })
-
-vim.keymap.set("n", "<leader>r", [["_ddP]], { desc = "Replace current line with yanked text" })
-
 vim.keymap.set("n", "<leader>ts", function()
     if vim.o.background == "dark" then
         vim.o.background = "light"
@@ -22,14 +18,6 @@ vim.keymap.set("n", "<leader>ts", function()
         print("Switched to dark mode")
     end
 end, { desc = "Toggle between light and dark mode" })
-
-vim.keymap.set("n", "<leader>my", function()
-    -- Enter insert mode and trigger completion
-    vim.cmd("startinsert")
-    vim.schedule(function()
-        require("cmp").complete()
-    end)
-end)
 
 local function dive_with_count_object()
     local count = vim.v.count1 -- Gets the count prefix (defaults to 1)
