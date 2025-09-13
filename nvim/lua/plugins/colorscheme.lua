259acce323fc7eeaa9679097aafd7b81ef2913ed
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-09-13 10:05:24 +0200

    Update colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,14 @@
 return {
     {
-        "folke/tokyonight.nvim",
-        enabled = false,
+        "sainnhe/everforest",
+        -- lazy = false,
+        -- priority = 1000,
+        config = function()
+            vim.g.everforest_background = "hard"
+            vim.g.everforest_enable_italic = false
+            vim.g.everforest_better_performance = 1
+            -- vim.cmd.colorscheme("everforest")
+        end,
     },
     {
         "catppuccin/nvim",
@@ -21,17 +28,6 @@ return {
         end,
     },
     {
-        "sainnhe/everforest",
-        -- lazy = false,
-        -- priority = 1000,
-        config = function()
-            vim.g.everforest_background = "hard"
-            vim.g.everforest_enable_italic = true
-            vim.g.everforest_better_performance = 1
-            -- vim.cmd.colorscheme("everforest")
-        end,
-    },
-    {
         "Mofiqul/vscode.nvim",
         config = function()
             vim.o.background = "dark"
@@ -39,25 +35,6 @@ return {
                 background = "hard",
             })
             -- vim.cmd.colorscheme("vscode")
-        end,
-    },
-    {
-        "olivercederborg/poimandres.nvim",
-        config = function()
-            require("poimandres").setup({
-                bold_vert_split = false,
-                dim_nc_background = false,
-                disable_background = false,
-                disable_float_background = false,
-                disable_italics = false,
-            })
-            -- vim.cmd.colorscheme("poimandres")
-        end,
-    },
-    {
-        "rose-pine/neovim",
-        config = function()
-            -- vim.cmd("colorscheme kanagawa-dragon")
         end,
     },
 }
