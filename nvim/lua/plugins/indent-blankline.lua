e8bd80e2d97ad76e848b55d0e0c487e9b7d10895
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-09-13 10:06:03 +0200

    Update indent blankline

---
+++ nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,37 @@
+return {
+    "lukas-reineke/indent-blankline.nvim",
+    event = "LazyFile",
+    opts = function()
+        return {
+            indent = {
+                char = "▏",
+                tab_char = "▏",
+            },
+            scope = {
+                enabled = true,
+                highlight = { "Label" },
+                show_start = true,
+                char = "▏",
+            },
+            exclude = {
+                filetypes = {
+                    "Trouble",
+                    "alpha",
+                    "dashboard",
+                    "help",
+                    "lazy",
+                    "mason",
+                    "neo-tree",
+                    "notify",
+                    "snacks_dashboard",
+                    "snacks_notif",
+                    "snacks_terminal",
+                    "snacks_win",
+                    "toggleterm",
+                    "trouble",
+                },
+            },
+        }
+    end,
+    main = "ibl",
+}
