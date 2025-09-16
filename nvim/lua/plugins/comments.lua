55db045040322c893af22c4f1e2201528b948326
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-09-16 09:30:40 +0200

    Update comments

--- nvim/lua/plugins/comments.lua
+++ nvim/lua/plugins/comments.lua
@@ -1,12 +1,4 @@
 return {
-  {
-    "numToStr/Comment.nvim",
-    opts = {
-      -- add any options here
-    },
-  },
-  {
-    "JoosepAlviste/nvim-ts-context-commentstring",
-    opts = { enable = true },
-  },
+    { "numToStr/Comment.nvim" },
+    { "JoosepAlviste/nvim-ts-context-commentstring" },
 }
