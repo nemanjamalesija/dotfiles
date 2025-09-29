db09ea6d4b36dd94827eec04bb099e5d19ecd274
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-09-29 22:13:24 +0200

    Update lsp to format on save

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -20,38 +20,6 @@ return {
             })
         end,
     },
-    -- {
-    --     "MunifTanjim/eslint.nvim",
-    --     dependencies = { "nvim-lua/plenary.nvim" },
-    --     config = function()
-    --         require("eslint").setup({
-    --             code_actions = {
-    --                 enable = true,
-    --                 apply_on_save = {
-    --                     enable = true,
-    --                     types = { "directive", "problem", "suggestion", "layout" },
-    --                 },
-    --             },
-    --             diagnostics = {
-    --                 enable = true,
-    --                 report_unused_disable_directives = false,
-    --                 run_on = "type",
-    --             },
-    --             -- FIXED: Explicitly exclude Vue files
-    --             filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
-    --             -- Prevent ESLint from attaching to Vue files
-    --             on_attach = function(client, bufnr)
-    --                 local ft = vim.api.nvim_buf_get_option(bufnr, "filetype")
-    --                 if ft == "vue" then
-    --                     vim.schedule(function()
-    --                         vim.lsp.buf_detach_client(bufnr, client.id)
-    --                     end)
-    --                     return false
-    --                 end
-    --             end,
-    --         })
-    --     end,
-    -- },
     {
         "neovim/nvim-lspconfig",
         opts = {
@@ -64,31 +32,28 @@ return {
             local capabilities = vim.lsp.protocol.make_client_capabilities()
             capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-            local ts_error = require("ts-error-translator")
-            ts_error.setup()
-
             -- FIXED: Create buffer-specific autocmd groups to prevent conflicts
             local on_attach = function(client, bufnr)
                 client.server_capabilities.documentFormattingProvider = true
                 client.server_capabilities.documentRangeFormattingProvider = true
-
-                -- Use buffer-specific group name
-                local group_name = "LspFormatOnSave_" .. bufnr
-                local group = vim.api.nvim_create_augroup(group_name, { clear = true })
-
-                vim.api.nvim_create_autocmd("BufWritePre", {
-                    group = group,
-                    buffer = bufnr,
-                    callback = function()
-                        local view = vim.fn.winsaveview()
-                        vim.lsp.buf.format({ bufnr = bufnr, async = false })
-                        vim.fn.winrestview(view)
-                    end,
-                })
             end
 
             local vue_ls_path = vim.fn.expand("$MASON/packages/vue-language-server")
             local vue_plugin_path = vue_ls_path .. "/node_modules/@vue/language-server"
+
+            require("lspconfig").eslint.setup({
+                capabilities = capabilities,
+                on_attach = function(client, bufnr)
+                    on_attach(client, bufnr)
+
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        buffer = bufnr,
+                        callback = function()
+                            vim.cmd("EslintFixAll")
+                        end,
+                    })
+                end,
+            })
 
             -- Stylelint
             vim.lsp.config.stylelint_lsp = {
@@ -101,22 +66,6 @@ return {
                     },
                 },
                 filetypes = { "css", "scss", "less", "sass" },
-            }
-
-            -- TypeScript with Vue plugin (Takeover Mode)
-            vim.lsp.config.ts_ls = {
-                capabilities = capabilities,
-                on_attach = on_attach,
-                init_options = {
-                    plugins = {
-                        {
-                            name = "@vue/typescript-plugin",
-                            location = vue_plugin_path,
-                            languages = { "vue" },
-                        },
-                    },
-                },
-                filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
             }
 
             -- HTML LSP
@@ -150,8 +99,23 @@ return {
                     "html",
                     "css",
                     "scss",
-                    "vue",
                 },
+            }
+
+            -- TypeScript with Vue plugin (Takeover Mode)
+            vim.lsp.config.ts_ls = {
+                capabilities = capabilities,
+                on_attach = on_attach,
+                init_options = {
+                    plugins = {
+                        {
+                            name = "@vue/typescript-plugin",
+                            location = vue_plugin_path,
+                            languages = { "vue" },
+                        },
+                    },
+                },
+                filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
             }
 
             -- Lua LSP
