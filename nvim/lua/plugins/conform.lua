36f2b540c924e352e33f934e416cb189017a053f
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-09-30 07:58:45 +0200

    Throw error when prettier fails

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,6 +1,17 @@
 vim.keymap.set("n", "<leader>fw", function()
     local pos = vim.api.nvim_win_get_cursor(0)
-    vim.cmd("silent %!prettier --stdin-filepath %")
+    local buf = vim.api.nvim_get_current_buf()
+    local filepath = vim.api.nvim_buf_get_name(buf)
+    local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+    local text = table.concat(lines, "\n")
+
+    local output = vim.fn.systemlist("prettier --stdin-filepath " .. vim.fn.shellescape(filepath), text)
+    if vim.v.shell_error == 0 and #output > 0 then
+        vim.api.nvim_buf_set_lines(buf, 0, -1, false, output)
+    else
+        vim.notify("Prettier failed.", vim.log.levels.ERROR)
+    end
+
     vim.api.nvim_win_set_cursor(0, pos)
 end, { desc = "Format buffer with prettier (no save, keep cursor)" })
 
