ef995039c503f529fb8256dd6dea14b92208b0f7
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-10-19 00:40:56 +0200

    Update copilot

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -2,12 +2,6 @@ return {
     {
         "zbirenbaum/copilot.lua",
         event = "InsertEnter",
-        config = function()
-            require("copilot").setup({
-                suggestion = { enabled = false },
-                panel = { enabled = false },
-            })
-        end,
     },
     {
         "zbirenbaum/copilot-cmp",
