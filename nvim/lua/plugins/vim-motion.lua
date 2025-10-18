81998759ee79bed41b97d6c96673d92abdc0a799
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-10-18 22:20:19 +0200

    Update bindings for vim motion

--- nvim/lua/plugins/vim-motion.lua
+++ nvim/lua/plugins/vim-motion.lua
@@ -1,8 +1,18 @@
 return {
     "easymotion/vim-easymotion",
+    keys = {
+        { "<leader>w", "<Plug>(easymotion-prefix)", mode = { "n", "x", "o" }, desc = "EasyMotion" },
+        { "<leader>e", "<Plug>(easymotion-prefix)", mode = { "n", "x", "o" }, desc = "EasyMotion" },
+        { "<leader>b", "<Plug>(easymotion-prefix)", mode = { "n", "x", "o" }, desc = "EasyMotion" },
+        { "<leader>k", "<Plug>(easymotion-prefix)", mode = { "n", "x", "o" }, desc = "EasyMotion" },
+        { "<leader>j", "<Plug>(easymotion-prefix)", mode = { "n", "x", "o" }, desc = "EasyMotion" },
+    },
     init = function()
-        vim.api.nvim_set_keymap("n", "<leader>w", "<Plug>(easymotion-prefix)", {})
-        vim.api.nvim_set_keymap("x", "<leader>w", "<Plug>(easymotion-prefix)", {})
-        vim.api.nvim_set_keymap("o", "<leader>w", "<Plug>(easymotion-prefix)", {})
+        pcall(function()
+            vim.keymap.del("n", "<leader>e")
+            vim.keymap.del("n", "<leader>b")
+            vim.keymap.del("x", "<leader>e")
+            vim.keymap.del("o", "<leader>e")
+        end)
     end,
 }
