3c4f59e28efc96cbaf686497d0c1a2f00ccb5f01
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-11-03 20:57:52 +0100

    Update tresitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -30,14 +30,5 @@ return {
             "yaml",
             "twig",
         })
-        opts.incremental_selection = {
-            enable = true,
-            keymaps = {
-                init_selection = "<C-space>",
-                node_incremental = "<C-space>",
-                scope_incremental = false,
-                node_decremental = "<bs>",
-            },
-        }
     end,
 }
