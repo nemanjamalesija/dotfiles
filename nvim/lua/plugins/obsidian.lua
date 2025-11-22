a3efab247e9fe6caffdca8f5a7abf498bce6b9b3
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-11-22 09:01:44 +0100

    Lazy load obsidian

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -1,7 +1,14 @@
 return {
     "epwalsh/obsidian.nvim",
     version = "*",
-    lazy = false,
+    lazy = true, -- Changed to true
+    cmd = {
+        "ObsidianOpen",
+        "ObsidianNew",
+        "ObsidianQuickSwitch",
+        "ObsidianSearch",
+        "ObsidianWorkspace",
+    },
     dependencies = {
         "nvim-lua/plenary.nvim",
     },
@@ -10,6 +17,9 @@ return {
             { name = "njuskalo", path = "~/vaults/njuskalo" },
             { name = "emasys", path = "~/vaults/emasys" },
             { name = "personal", path = "~/vaults/personal" },
+        },
+        ui = {
+            enable = false,
         },
     },
 }
