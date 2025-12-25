26934aa51339e14756b47b43fe64a5720174ac7d
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-12-26 00:08:11 +0100

    Upate blink

--- nvim/lua/plugins/blink-cmp.lua
+++ nvim/lua/plugins/blink-cmp.lua
@@ -1,9 +1,31 @@
 return {
     "saghen/blink.cmp",
     version = "1.*",
+    dependencies = {
+        "zbirenbaum/copilot.lua",
+        {
+            "giuxtaposition/blink-cmp-copilot",
+        },
+    },
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
+        sources = {
+            default = { "lsp", "path", "snippets", "buffer", "copilot" },
+            providers = {
+                copilot = {
+                    name = "copilot",
+                    module = "blink-cmp-copilot",
+                    score_offset = 100,
+                    async = true,
+                },
+            },
+        },
+        completion = {
+            ghost_text = {
+                enabled = false,
+            },
+        },
         keymap = {
             preset = "default",
             ["<C-e>"] = { "hide" },
@@ -11,6 +33,8 @@ return {
             ["<C-k>"] = { "select_prev", "fallback" },
             ["<C-f>"] = { "scroll_documentation_up", "fallback" },
             ["<C-v>"] = { "scroll_documentation_down", "fallback" },
+            ["<C-l>"] = { "snippet_forward", "fallback" },
+            ["<C-h>"] = { "snippet_backward", "fallback" },
         },
     },
 }
