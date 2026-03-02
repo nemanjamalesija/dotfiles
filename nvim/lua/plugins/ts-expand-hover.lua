818aaafa2b08dcef3c35fa94087466eb732b027c
Nemanja Malesija <malesija.nemanja@gmail.com>
2026-03-02 08:44:49 +0100

    Add ts expand hover plugin

---
+++ nvim/lua/plugins/ts-expand-hover.lua
@@ -0,0 +1,8 @@
+return {
+    "nemanjamalesija/ts-expand-hover.nvim",
+    ft = { "typescript", "typescriptreact" },
+    main = "ts_expand_hover",
+    opts = {
+        keymaps = { hover = "<leader>th" },
+    },
+}
