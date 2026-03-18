777024ca05a4b15d2b4d38af4828e583dc54d496
Nemanja Malesija <malesija.nemanja@gmail.com>
2026-03-18 10:11:29 +0100

    Add magenta cursor

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -8,3 +8,17 @@ vim.opt.termguicolors = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.cmdheight = 0
+
+-- Cursor color (magenta) - use autocommand so it persists after colorscheme changes
+vim.opt.guicursor = "n-v-c-sm:block-Cursor,i-ci-ve:ver25-Cursor,r-cr-o:hor20-Cursor"
+
+local function set_cursor_hl()
+    vim.api.nvim_set_hl(0, "Cursor", { fg = "#1e1e2e", bg = "#bf68d9" })
+    vim.api.nvim_set_hl(0, "lCursor", { fg = "#1e1e2e", bg = "#bf68d9" })
+end
+
+set_cursor_hl()
+vim.api.nvim_create_autocmd("ColorScheme", {
+    pattern = "*",
+    callback = set_cursor_hl,
+})
