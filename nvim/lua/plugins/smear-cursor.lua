f5f6828d13e6dc43837551720ebe2b830d4339b4
Nemanja Malesija <malesija.nemanja@gmail.com>
2026-03-24 00:26:03 +0100

    Update smear cursor config

--- nvim/lua/plugins/smear-cursor.lua
+++ nvim/lua/plugins/smear-cursor.lua
@@ -2,5 +2,9 @@ return {
     "sphamba/smear-cursor.nvim",
     opts = {
         cursor_color = "#bf68d9",
+        stiffness = 0.8,
+        trailing_stiffness = 0.6,
+        damping = 0.95,
+        distance_stop_animating = 0.5,
     },
 }
